1631d7e7f6163917063d06d23a7b2f2485ddcf0d
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-18 00:52:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |age, name|
     if age <=> 12 puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
+  greeting = name
   end
 
 end
